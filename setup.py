204865489e1143015b2af4b8388bab63200e2a9a
Michiaki Ariga <chezou+github@gmail.com>
2015-05-13 00:06:35 +0900

    Modify README

--- setup.py
+++ setup.py
@@ -6,14 +6,19 @@ except ImportError:
     from distutils.core import setup, Extension
 
 ext_module = Extension('_Mykytea',
-                       sources=['mykytea_wrap.cxx', 'mykytea.cpp'],
+                       sources=['lib/kytea/mykytea_wrap.cxx', 'lib/kytea/mykytea.cpp'],
                        libraries=["kytea"],
                        )
 
-setup(name='kytea-python',
-      version='0.1',
-      author='chezou',
+setup(
+      name='kytea',
+      version='0.1.0',
+      author='Michiaki Ariga',
       author_email='chezou@gmail.com',
+      description = ('An text analysis toolkit KyTea binding'),
+      license = 'MIT',
+      keywords = 'KyTea NLP',
+      url = 'https://github.com/chezou/Mykytea-python',
       ext_modules=[ext_module],
-      py_modules=['Mykytea'],
+      py_modules=['lib/kytea/Mykytea'],
       )
