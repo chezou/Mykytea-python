39df8c741af9fc94bc64db737af2bc7ad62b18f2
Michiaki Ariga <chezou+github@gmail.com>
2015-05-17 20:51:05 +0900

    Remove model argument in test

--- mykytea_test.py
+++ mykytea_test.py
@@ -21,7 +21,10 @@ def list_tags(t):
         return (t2[0], type(t2[1]))
     return [(word.surface, [[convert(t2) for t2 in t1] for t1 in word.tag]) for word in t]
 
-opt = "-model /usr/local/share/kytea/model.bin -deftag UNKNOWN!!"
+# You can pass arguments KyTea style like following
+opt = "-deftag UNKNOWN!!"
+# You can also set your own model
+#opt = "-model /usr/local/share/kytea/model.bin"
 mk = Mykytea.Mykytea(opt)
 
 s = "今日はいい天気です。1999"
@@ -54,7 +57,7 @@ class SimpleTest(unittest.TestCase):
         """解析結果を文字列で取得"""
         self.assertEqual(mk.getTagsToString(s),
                          '今日/名詞/きょう は/助詞/は い/形容詞/い い/語尾/い 天気/名詞/てんき で/助動詞/で す/語尾/す 。/補助記号/。 1999/名詞/１９９９ ')
-        
+
     def test_get_tags(self):
         """1位のタグを取得"""
         self.assertEqual(list_tags(mk.getTags(s)),
