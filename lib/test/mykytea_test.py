6adfbb68181b73c11b87bfb3cc425385019506e9
Michiaki Ariga <chezou+github@gmail.com>
2016-03-28 21:51:25 +0900

    Add print brace

--- lib/test/mykytea_test.py
+++ lib/test/mykytea_test.py
@@ -14,7 +14,7 @@ def showTags(t):
                     out = out + "/" + str(t3)
                 out += "\t"
             out += "\t"
-        print out
+        print(out)
 
 def list_tags(t):
     def convert(t2):
@@ -31,10 +31,10 @@ s = "今日はいい天気です。1999"
 
 #分かち書きを取得
 for word in mk.getWS(s):
-    print word
+    print(word)
 
 #解析結果を文字列で取得
-print mk.getTagsToString(s)
+print(mk.getTagsToString(s))
 
 #1位のタグを取得
 t = mk.getTags(s)
