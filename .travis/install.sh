55b268cb8af417ec91a3aafb7885c5aae71b4351
Aki Ariga <chezou+github@gmail.com>
2017-11-18 21:05:40 +0900

    Move before install script into shell script

---
+++ .travis/install.sh
@@ -0,0 +1,16 @@
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew install swig;
+    brew outdated pyenv || brew upgrade pyenv;
+    brew install pyenv-virtualenv;
+    pyenv install $PYTHON;
+    export PYENV_VERSION=$PYTHON;
+    export PATH="/Users/travis/.pyenv/shims:${PATH}";
+    pyenv-virtualenv venv;
+    source venv/bin/activate;
+    python --version;
+fi
+wget http://www.phontron.com/kytea/download/kytea-0.4.7.tar.gz
+tar zxf kytea-0.4.7.tar.gz
+pushd kytea-0.4.7 && ./configure && make && sudo make install && popd
+rm -r kytea-0.4.7
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then sudo ldconfig; fi 
