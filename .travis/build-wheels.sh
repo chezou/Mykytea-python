ccf704ced1c7ce153603e830626fdf6a5576cb1d
Aki Ariga <chezou+github@gmail.com>
2017-11-19 21:05:00 +0900

    Use make clean insted of make distclean

--- .travis/build-wheels.sh
+++ .travis/build-wheels.sh
@@ -18,7 +18,7 @@ rm -rf /opt/python/cp33-cp33m
 #Make kytea
 cd /io/kytea-0.4.7
 autoreconf -i
-./configure && make distclean && make && make install
+./configure && make clean && make && make install
 ldconfig
 cd /io
 swig -Wall -c++ -python -shadow -I/usr/local/include /io/lib/kytea/mykytea.i
