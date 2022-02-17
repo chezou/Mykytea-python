5da68430aa1a2f72db2de3111110a3f14ec3b3a4
Aki Ariga <chezou+github@gmail.com>
2022-02-17 09:26:11 -0800

    Use setuptools_scm for version number

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import os
 import sys
 from setuptools import setup, Extension, find_packages
 
-is_windows = os.name == 'nt'
+is_windows = os.name == "nt"
 
 lib = ["libkytea"] if is_windows else ["kytea"]
 
-include_dirs = ['lib/kytea', 'include']
+include_dirs = ["lib/kytea", "include"]
 library_dirs = ["lib/kytea"]
 
 kytea_dir = os.environ.get("KYTEA_DIR")
@@ -16,41 +16,41 @@ if kytea_dir:
     include_dirs.append(os.path.join(kytea_dir, "include"))
     library_dirs.append(os.path.join(kytea_dir, "lib"))
 
-ext_module = Extension('_Mykytea',
-                       sources=['lib/kytea/mykytea_wrap.cxx', 'lib/kytea/mykytea.cpp'],
-                       library_dirs=library_dirs,
-                       libraries=lib,
-                       include_dirs=include_dirs
-                       )
+ext_module = Extension(
+    "_Mykytea",
+    sources=["lib/kytea/mykytea_wrap.cxx", "lib/kytea/mykytea.cpp"],
+    library_dirs=library_dirs,
+    libraries=lib,
+    include_dirs=include_dirs,
+)
 
-libdir = 'lib/kytea'
-sys.path.append('./lib/kytea')
-sys.path.append('./lib/test')
-VERSION = "0.1.7"
+libdir = "lib/kytea"
+sys.path.append("./lib/kytea")
+sys.path.append("./lib/test")
 
 setup(
-      name='kytea',
-      version=VERSION,
-      author='Michiaki Ariga',
-      author_email='chezou@gmail.com',
-      description=('An text analysis toolkit KyTea binding'),
-      long_description=open('README.md').read(),
-      long_description_content_type="text/markdown",
-      license='MIT License',
-      keywords=['KyTea', 'NLP', 'Japanese morphological analyzer'],
-      url='https://github.com/chezou/Mykytea-python',
-      download_url='https://github.com/chezou/Mykytea-python/tarball/{}'.format(VERSION),
-      classifiers=[
+    name="kytea",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    author="Aki Ariga",
+    author_email="chezou@gmail.com",
+    description=("An text analysis toolkit KyTea binding"),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT License",
+    keywords=["KyTea", "NLP", "Japanese morphological analyzer"],
+    url="https://github.com/chezou/Mykytea-python",
+    classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Topic :: Text Processing :: Linguistic",
-      ],
-      ext_modules=[ext_module],
-      packages=find_packages(libdir),
-      package_dir={"": libdir},
-      py_modules=['Mykytea'],
-      test_suite='mykytea_test.suite'
-      )
+    ],
+    ext_modules=[ext_module],
+    packages=find_packages(libdir),
+    package_dir={"": libdir},
+    py_modules=["Mykytea"],
+    test_suite="mykytea_test.suite",
+)
